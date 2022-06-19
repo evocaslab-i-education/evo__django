4e1ece2dd868b68572fca6fdefa0850c7b75b339
alirex <alirex.prime@gmail.com>
2022-06-19 16:07:31 +0300

    ♻️Rework

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# shellcheck disable=SC2124
 cmd="$@"
 
 exec $cmd
--- docker/app/start.sh
+++ docker/app/start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-#python manage.py migrate
 make migrate
 
 python manage.py runserver 0.0.0.0:8000
