25bf2ed0a1a4699c937eaa0736c05b2ec989340c
alirex <alirex.prime@gmail.com>
2022-06-23 21:44:03 +0300

    ✨ Add admin and relations

--- apps/humans/admin.py
+++ apps/humans/admin.py
@@ -1,1 +1,25 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Human, Color, SuperHuman
+
+admin.site.register(Human)
+
+
+# admin.site.register(Color)
+# admin.site.register(SuperHuman)
+
+
+class SuperHumanInlineAdmin(admin.TabularInline):
+    model = SuperHuman
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    inlines = (
+        SuperHumanInlineAdmin,
+    )
+
+
+@admin.register(SuperHuman)
+class SuperHumanAdmin(admin.ModelAdmin):
+    ...
--- apps/humans/models.py
+++ apps/humans/models.py
@@ -2,9 +2,72 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 
+# class User:
+#     ...
+#
+#
+# class Expert:
+#     ...
+#
+#
+# class Speciality:
+#     ...
+#
+#
+# class Procedure:
+#     ...
+
+
+class ColorsChoices(models.TextChoices):
+    BLACK = 'black', 'Black'
+    WHITE = 'white', 'White'
+    RED = 'red', 'Red'
+
+
+class Color(models.Model):
+    name = models.CharField('Name', help_text='Name of color', max_length=200)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+    __repr__ = __str__
+
+
 class Human(models.Model):
     name = models.CharField('Name', help_text='It is name of human', max_length=200)
     age = models.PositiveSmallIntegerField('Age', help_text='How old this human', validators=[MaxValueValidator(150)])
 
+    favourite_color = models.CharField(
+        'Favourite color',
+        max_length=32,
+        choices=ColorsChoices.choices,
+        default=ColorsChoices.WHITE,
+    )
+
+    favourite_color_by_foreign_key = models.ForeignKey(
+        Color,
+        related_name='humans_related_by_foreign_key',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    favourite_color_many_to_many = models.ManyToManyField(
+        Color,
+        related_name='humans_related_by_many_to_many',
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.age}'
+
+    __repr__ = __str__
+
+    # def __repr__(self) -> str:
+    #     return str(self)
+
+
+class SuperHuman(Human):
+    level = models.PositiveIntegerField('Level', help_text='Level of power')
